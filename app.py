88a656a0a165c344bf9f80449788c496e4683589
Jimmy Huang <pocper1@gmail.com>
2021-12-12 01:24:10 +0800

    add flask empty structure

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, url_for,abort,request
+app = Flask(__name__)
+# import linebot related
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    LocationSendMessage, ImageSendMessage, StickerSendMessage,
+    VideoSendMessage, TemplateSendMessage, ButtonsTemplate, PostbackAction, MessageAction, URIAction,
+    PostbackEvent, ConfirmTemplate, CarouselTemplate, CarouselColumn,
+    ImageCarouselTemplate, ImageCarouselColumn, FlexSendMessage
+)
+import json
+app = Flask(__name__)
+line_bot_api = LineBotApi('YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        print('receive msg')
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+    return 'OK'
+    # handle msg
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    # get user info & message
+    user_id = event.source.user_id
+    msg = event.message.text
+    user_name = line_bot_api.get_profile(user_id).display_name
+    
+    # get msg details
+    print('msg from [', user_name, '](', user_id, ') : ', msg)
+
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text = msg))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
+        
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000, debug=True)
