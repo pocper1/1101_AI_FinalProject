cd6cb852b590183de1a724360740a4ec2d2770cc
WeiFan-Wang <91296365+WeiFan-Wang@users.noreply.github.com>
2021-12-22 00:17:13 +0800

    未完成

--- recommend.py
+++ recommend.py
@@ -22,8 +22,22 @@ url='http://www.w3.org/TR/html4/strict.dtd'
 # api 網址
 # https://www.books.com.tw/web/books
 
+def get_books_list():
+    res = requests.get(get_url(bookName), headers=header)
+    res.encoding = 'utf-8'     #轉換編碼至UTF-8
+    rawdata = res.json()
+    
+        
+    # data = json.loads(res)
+    for i in range(0,25):
+        print("推薦書籍:")
+        print("第 "+str(i)+" 本")
+        print("書名: "+str(rawdata["docs"][i]["pnx"]["display"]["title"][0]))
+        isbn = "isbn" in rawdata["docs"][i]["pnx"]["addata"]
+        
+            
 
 # 使用者輸入參數
 bookName=input('推薦書籍:')
 
-result = get_books_list()
+result = 
