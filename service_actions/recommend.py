db7d1ee4c1cd6efbc7b72df2a2bef991aa7e2476
WeiFan-Wang <91296365+WeiFan-Wang@users.noreply.github.com>
2022-01-13 03:56:07 +0800

    Update recommend.py

--- service_actions/recommend.py
+++ service_actions/recommend.py
@@ -1,16 +1,24 @@
+# 載入所需的工具
 import requests
-from lxml import etree
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"}
+from bs4 import BeautifulSoup
+#import time
+#import pandas as pd
 
 
-def receive_recomend():
-  res = requests.get("http://www.books.com.tw/web/sys_hourstop/home?loc=P_003_001",headers=headers)
-  content = res.content.decode()
+bookName = input("請輸入要查詢的書籍：")
 
-  html = etree.HTML(content)
-  title = html.xpath('//body/div[4]/div/div[2]/div[1]/div/div[1]/ul/li/div[2]/h4/a/text()')
-  price = html.xpath('//body/div[4]/div/div[2]/div[1]/div/div[1]/ul/li/div[2]/ul/li/strong[last()]/b/text()')
+# 取得查詢書籍頁面
+resp = requests.get("https://search.books.com.tw/search/query/key/" + bookName +"/cat/all")
+soup = BeautifulSoup(resp.text, 'html.parser')
 
-  print ("推薦書籍:")
-  for i,j in zip(title,price):
-    print(i,j)
+# 取得書籍位置
+info_items = soup.find_all('a', target="_blank", rel="mid_name")
+for item in info_items:
+    name = item.get('title')
+    print(name)
+  
+info_items = soup.find_all('a', rel="go_author")
+for item in info_items:
+    author = item.get('title')
+    print(author)
+  
